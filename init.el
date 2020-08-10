537f8825fb11b865c267ce283175e1f9a28c674e
Alexandr Timchenko <Alexandr.Timchenko@sivis.com>
2020-08-10 14:15:07 +0200

    Back to theme-hopping!
    * Trying out `modus-operandi-theme` for now
    * Removed some unused "haX"
    * Added mvn to the list of esh-visual commands.

--- init.el
+++ init.el
@@ -366,7 +366,7 @@
   ;; Visual commands are commands which require a proper terminal
   ;; eshell will run them in a term buffer when you invoke them.
   (eshell-visual-commands
-   '("pip" "pipenv" "npm" "tmux" "htop" "top"))
+   '("pip" "pipenv" "npm" "mvn" "tmux" "htop" "top"))
   (eshell-visual-subcommands
    '(("git" "log" "l" "diff" "show"))))
 
@@ -974,16 +974,16 @@
 (set-frame-parameter nil 'alpha '(95 . 75))
 (fringe-mode 10)
 
-;; Functions
-(defun powershell (&optional buffer)
-  "Launches a powershell in buffer *powershell* and switches to it."
-  (interactive)
-  (let ((buffer (or buffer "*powershell*"))
-        (powershell-prog "c:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"))
-    (make-comint-in-buffer "shell" "*powershell*" powershell-prog)
-    (switch-to-buffer buffer)
-    (visual-line-mode 1)))
+(use-package modus-operandi-theme
+  :ensure t
+  :config
+  (load-theme 'modus-operandi t)
+  :custom
+  (modus-operandi-theme-org-blocks 'rainbow)
+  (modus-operandi-theme-slanted-constructs t)
+  (modus-operandi-theme-bold-constructs t))
 
+;; My custom functions
 (defun my/insert-change-id ()
   "Generate change Id on the third line of the current buffer (gerrit)."
   (interactive)
@@ -1019,36 +1019,3 @@
     (start-process-shell-command pr-name
                                  "*VLC-Log*"
                                  shell-command)))
-
-;; Hack for setting a fixed wrap column in visual-line-mode.
-(defun set-visual-wrap-column (new-wrap-column &optional buffer)
-  "Force visual line wrap at NEW-WRAP-COLUMN in BUFFER (defaults
-    to current buffer) by setting the right-hand margin on every
-    window that displays BUFFER.  A value of NIL or 0 for
-    NEW-WRAP-COLUMN disables this behavior."
-  (interactive (list (read-number "New visual wrap column, 0 to disable: " (or visual-wrap-column fill-column 0))))
-  (if (and (numberp new-wrap-column)
-           (zerop new-wrap-column))
-      (setq new-wrap-column nil))
-  (with-current-buffer (or buffer (current-buffer))
-    (visual-line-mode t)
-    (set (make-local-variable 'visual-wrap-column) new-wrap-column)
-    (add-hook 'window-configuration-change-hook 'update-visual-wrap-column nil t)
-    (let ((windows (get-buffer-window-list)))
-      (while windows
-        (when (window-live-p (car windows))
-          (with-selected-window (car windows)
-            (update-visual-wrap-column)))
-        (setq windows (cdr windows))))))
-
-(defun update-visual-wrap-column ()
-  (if (not visual-wrap-column)
-      (set-window-margins nil nil)
-    (let* ((current-margins (window-margins))
-           (right-margin (or (cdr current-margins) 0))
-           (current-width (window-width))
-           (current-available (+ current-width right-margin)))
-      (if (<= current-available visual-wrap-column)
-          (set-window-margins nil (car current-margins))
-        (set-window-margins nil (car current-margins)
-                            (- current-available visual-wrap-column))))))
